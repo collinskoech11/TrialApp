44a401e71469b161916d5f8d37b6ccbfdd7db7f8
Collins Koech <collinskoechck34@gmail.com>
2020-10-15 11:30:21 +0300

    start button added

--- App.js
+++ App.js
@@ -5,17 +5,20 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text >Open up App.js to start working</Text>
+      <Text>Welcome</Text>
+      <Button>Get Started</Button>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'navy',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
